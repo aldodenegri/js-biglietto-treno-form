94e93172ac562dbb382324967118798e299a5e3e
aldodenegri <aldodenegri00@gmail.com>
2025-11-12 23:41:08 +0100

    Aggiunto calcolo per lo sconto

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
-document.getElementById("submit").addEventListener("click", function(event) {
+document.getElementById("submit").addEventListener("click", function (event) {
     event.preventDefault(); // ← Impedisce il ricaricamento della pagina
     storeData();
 });
 
 document.getElementById("cancel").addEventListener("click", cancelInput);
+const prezzoKm = 0.21;
 
 function storeData() {
-  let nc= document.getElementById("NC").value;
-  let km= document.getElementById("km").value;
-  console.log(nc + " " + km);
+    
+    let nc = document.getElementById("NC").value;
+    let km = document.getElementById("km").value;
+    let eta = document.getElementById("eta").value;
+    let prezzoTotale = prezzoKm * km;
+    let prezzoScontato = prezzoTotale;
+    if (eta < 18) {
+        prezzoScontato = prezzoTotale * 0.8; // 20% di sconto
+    } else if (eta >= 65) {
+        prezzoScontato = prezzoTotale * 0.6; // 40% di sconto
+    }
+    console.log(nc + " " + km + " " + prezzoScontato.toFixed(2));
 }
 
-function cancelInput(){
-    document.getElementById("NC").value="";
-    document.getElementById("km").value="";
+function cancelInput() {
+    document.getElementById("NC").value = "";
+    document.getElementById("km").value = "";
     console.log("Valori cancellati");
 }
+
+
+
+
+
+
+//const divRisultato = document.getElementById("risultato-somma");
+//divRisultato.textContent = "Prezzo da pagare: € " + prezzoScontato.toFixed(2);
