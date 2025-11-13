d0c5c2729746d6c9fa363266864fe2530bfe9333
aldodenegri <aldodenegri00@gmail.com>
2025-11-13 11:30:47 +0100

    Aggiunto css/bootsrap-css

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ document.getElementById("submit").addEventListener("click", function (event) {
 
 document.getElementById("cancel").addEventListener("click", cancelInput);
 const prezzoKm = 0.21;
-
+const valDiv= document.getElementById("values");
 function storeData() {
 
     const nc = document.getElementById("NC").value.trim();
     const km = parseFloat(document.getElementById("km").value);
     const eta = parseInt(document.getElementById("eta").value);
-
+    
     if (!nc || isNaN(km) || isNaN(eta) || km <= 0 || eta <= 0) {
         alert("Compila tutti i campi correttamente!");
         return;
@@ -30,8 +30,8 @@ function storeData() {
     } else {
         sconto = "Prezzo pieno";
     }
-
-    document.getElementById("values").innerHTML = `
+    
+    valDiv.innerHTML = `
         <h3>Biglietto Treno</h3>
         <ul>
             <li><strong>Passeggero:</strong> ${nc}</li>
@@ -41,19 +41,15 @@ function storeData() {
             <li><strong>Prezzo finale:</strong> €${prezzoScontato.toFixed(2)}</li>
         </ul>
     `;
+    valDiv.classList.remove("d-none");
     console.log(nc, km, eta, "€" + prezzoScontato.toFixed(2));
 }
 
 function cancelInput() {
     document.getElementById("NC").value = "";
     document.getElementById("km").value = "";
+    document.getElementById("eta").value="";
+    valDiv.classList.add("d-none");
     console.log("Valori cancellati");
+
 }
-
-
-
-
-
-
-//const divRisultato = document.getElementById("risultato-somma");
-//divRisultato.textContent = "Prezzo da pagare: € " + prezzoScontato.toFixed(2);
